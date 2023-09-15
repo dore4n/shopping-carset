6e7e2b582a77103ede71809c18a8d45dbc1e8915
dore4n <lucasebsantos0@gmail.com>
2023-09-15 01:13:09 -0300

    feat: endpoint api mercadolivre

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,29 @@
+import propTypes from 'prop-types';
+import React from 'react';
+import { BiSolidCart } from 'react-icons/bi';
+import formatCurrency from '../../utils/formatCurrency';
+import './ProductCard.css';
+
+function ProductCard ({ data }) {
+    const {title, thumbnail, price } = data;
+    return (
+        <section className="product-card">
+            <img src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')} alt="product" className="card__image" />
+            <div className="card__infos">
+                <h2 className="card__price">{formatCurrency(price, 'BRL')}</h2>
+                <h2 className="card__title">{title}</h2>
+            </div>
+
+            <button type="button" className="button__add-cart">
+                <BiSolidCart />
+            </button>
+        </section>
+    );
+}
+
+export default ProductCard;
+
+ProductCard.propTypes = {
+    data: propTypes.shape({})
+
+}.isRequired;
